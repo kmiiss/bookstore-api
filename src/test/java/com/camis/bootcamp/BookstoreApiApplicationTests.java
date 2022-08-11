5e9e0a48c4539c1be13913b9523b2550c7ccc45e
kmiiss <piresdesouza97@gmail.com>
2022-08-11 17:28:45 -0300

    commit inicial

---
+++ src/test/java/com/camis/bootcamp/BookstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camis.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
